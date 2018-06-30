9106d4f5d9b3e1d36bfae7d89e4601887a9f1f8d
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-06-30 14:44:50 +0800

    bugfix

--- h5-action/config/index.js
+++ h5-action/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/didi-channel-coupon/',
+    assetsPublicPath: '/world-cup-sms/',
 
     /**
      * Source Maps
